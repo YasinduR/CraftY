7eac9d371a2c0f68476ea30751ac260841021806
YasinduR <YSNDRMT@GMAIL.COM>
2024-07-27 16:56:11 +0530

    Routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ProductsComponent } from './components/products/products.component';
-//import { PolicyComponent } from './components/policy/policy.component';
-//import { ProductsComponent } from './components/products/products.component';
+import { PolicyComponent } from './components/policy/policy.component';
+import { TosComponent } from './components/tos/tos.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
  { path: 'contact', component: ContactComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'policy', component: PolicyComponent },
+  { path: 'tos', component: TosComponent },
   { path: '**', redirectTo: '' }
 ];
 
