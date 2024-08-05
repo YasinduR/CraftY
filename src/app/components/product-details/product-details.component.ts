8453202c2fd5b424abe14b8274af11604fe70510
YasinduR <YSNDRMT@GMAIL.COM>
2024-08-05 11:56:30 +0530

    add/remove item functions in product detail page & cart component minor update

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { ProductService } from '../../services/product.service';
-//import { Product } from '../../models/product.model';
+import { CartService } from '../../services/cart.service';
 import { ProductDetail } from '../../models/product-detail.model';
+import { AuthService } from '../../services/auth.service';
+
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-product-details',
@@ -11,17 +15,22 @@ import { ProductDetail } from '../../models/product-detail.model';
 })
 export class ProductDetailsComponent implements OnInit {
   product: ProductDetail | undefined;
+  user: User | undefined;
+
   currentImageIndex: number = 0;
   isLoading = true;
   errorMessage = '';
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private authService: AuthService,
+    private cartSevice: CartService
   ) {}
 
   ngOnInit(): void {
     const ref = this.route.snapshot.paramMap.get('_id');
+    this.user = this.authService.getUserInfo(); 
     if(ref){
       this.productService.getProductById(ref).subscribe({
         next: (data) => {
@@ -49,5 +58,52 @@ export class ProductDetailsComponent implements OnInit {
     }
   }
 
+  AddToCart():void{
+    if(this.product && this.user){
+     const Cartitem= {objectId:this.product._id,count:1} // New Cartitem
+     const productId = this.product._id; // product id
+     const exists = this.is_already_in_Cart(); // existance of the product in the user cart
 
+      this.cartSevice.addToCart(this.user.email,this.product._id).subscribe({
+        next: (result) => {
+          if (!exists){
+            this.user?.cart.push(Cartitem);
+          }
+          console.log(result)
+        },
+        error: (error) => {
+          console.log(error)
+        }}
+      );
+    }
+    }
+
+    RemoveFromCart():void{
+      if(this.product && this.user){
+        const index_a = this.user?.cart.findIndex(item => item.objectId ===this.product?._id);
+        this.cartSevice.updateCart(this.user.email,this.product._id,0).subscribe({ // remove item => item count zero
+          next: (result) => {
+            if (index_a!==0){
+              this.user?.cart.splice(index_a,1);
+            }
+            console.log(result)
+          },
+          error: (error) => {
+            console.log(error)
+          }}
+        );
+      }
+      }
+
+
+
+  is_already_in_Cart():boolean | undefined{ // check whether the product is already on the cart
+    if (this.product && this.user){
+      const productId = this.product._id; // product id
+      return this.user.cart?.some(item => item.objectId ===productId)
+  }
+  else{
+    return undefined
+  }
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -7,17 +7,24 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class CartService {
+export class CartService { //Services related Cart of a user 
   //user: User | undefined;
-  private apiUrl = 'http://localhost:3000/user/update_cart'; // Replace with your actual API endpoint
+  private apiUrl = 'http://localhost:3000/user'; // Replace with your actual API endpoint
 
 
 
   constructor(private http: HttpClient  ) {}
 
-  updateCart(email:string, ObjectId: string,new_count: Number): Observable<any>{
-    console.log(email,ObjectId,new_count)
-    return this.http.post<any>(this.apiUrl,{email:email, objectId:ObjectId, quantity:new_count});
+  updateCart(email:string, ObjectId: string,new_count: Number): Observable<any>{ // this includes delete item
+    const url = `${this.apiUrl}/update_cart`;
+    return this.http.post<any>(url,{email:email, objectId:ObjectId, quantity:new_count});
   }
+
+  addToCart(email:string, ObjectId: string): Observable<any>{ // Add a item to cart
+    const url = `${this.apiUrl}/add_to_cart`
+    return this.http.post<any>(url,{email:email, objectId:ObjectId});
+  }
+
+  
 
 }
